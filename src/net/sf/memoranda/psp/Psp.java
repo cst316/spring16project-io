69e9071a4aba5710d1be0fe74c071819069ff8e5
Joshva Kappamamoottil <jkappama@asu.edu>
2016-02-17 11:56:47 -0800

    Psp interface and implementation.

---
+++ src/net/sf/memoranda/psp/Psp.java
@@ -0,0 +1,15 @@
+package net.sf.memoranda.psp;
+
+import net.sf.memoranda.date.CurrentDate;
+
+public interface Psp {
+	
+	final int pID = 1;
+
+	public CurrentDate getStDate();
+	public void setStDate(CurrentDate stDate);
+	public String getName();
+	public void setName(String name);
+	public String getDescription();
+	public void setDescription(String description);
+}
---
+++ src/net/sf/memoranda/psp/PspImpl.java
@@ -0,0 +1,65 @@
+package net.sf.memoranda.psp;
+
+import net.sf.memoranda.date.CurrentDate;
+
+public class PspImpl implements Psp {
+	
+	private int pID;
+	private CurrentDate stDate;
+	private String name;
+	private String description;
+	static int lastID;
+	
+	public PspImpl() {
+		stDate = null;
+		name = "";
+		description = "";
+		lastID = 0;
+	}
+	public PspImpl(CurrentDate stDate, String name, String description) {
+		super();
+		pID = Psp.pID;
+		this.stDate = stDate;
+		this.name = name;
+		this.description = description;
+	}
+	
+	public static int getpID() {
+		return Psp.pID;
+	}
+	public static void setpID(int pID) {
+		//Will change to accept a static file as a parameter 2/17/2016 - Josh K.
+		int theProjectID = Psp.pID;
+		theProjectID = pID;
+	}
+	public CurrentDate getStDate() {
+		return stDate;
+	}
+	public void setStDate(CurrentDate stDate) {
+		this.stDate = stDate;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public static int getLastID() {
+		return lastID;
+	}
+	public static void setLastID(int lastID) {
+		PspImpl.lastID = lastID;
+	}
+	@Override
+	public String toString() {
+		return "PSP Project Information:\n"
+				+ "Project ID = " + PspImpl.getpID() + ", Project Start Date = " + this.getStDate() 
+				+ ", Project Name=" + this.getName() + ", Project Description=" + this.getDescription();
+	}
+}
