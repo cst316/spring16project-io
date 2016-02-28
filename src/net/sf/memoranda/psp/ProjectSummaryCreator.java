d87734db8982d13eb8f85cb0c8e05f325c71c527
Julie Teplik <jteplik@asu.edu>
2016-02-27 23:16:31 -0700

    Almost finished. Trying to test but having trouble

--- src/net/sf/memoranda/psp/ProjectSummaryCreator.java
+++ src/net/sf/memoranda/psp/ProjectSummaryCreator.java
@@ -46,15 +46,16 @@ public class ProjectSummaryCreator {
 	 * Creates a PSP Summary Form with all specified data
 	 * 
 	 */
-	public static void createFileSummary(String author, String programName, String date, int [] estimatedTime, int [] actualTime){
+	public static void createFileSummary(String author, String programName, String date, double[] estimatedTime, double[] actualTime,
+			int[] actualDefects, double[] estimatedTotal, double[] actualTotal){
 		try {
 			Document document = new Document();
 		    PdfWriter.getInstance(document, new FileOutputStream(FILE));
 		    document.open();
-		    //do stuff to document
 		    addPDFTitle(document, author, programName, date);
-		    
 		    createTimeTable(estimatedTime, actualTime);
+		    createDefectTable(actualDefects);
+		    createSummaryTable(estimatedTotal, actualTotal);
 		    document.close();
 		}catch (Exception e) {
 			e.printStackTrace();
@@ -93,16 +94,24 @@ public class ProjectSummaryCreator {
 	 * @param estimatedTime the estimated times that it would take for a task
 	 * @param actualTime the actual time that it took for a task
 	 * 
-	 * Creates the table with Estimated Time, Actual Time, To Date, and
+	 * Creates the Time Table with Estimated Time, Actual Time, To Date, and
 	 * % To Date
 	 * 
 	 */
-	private static void createTimeTable(int [] estimatedTime, int [] actualTime){
+	private static void createTimeTable(double [] estimatedTime, double [] actualTime){
 		
 		Paragraph table1 = new Paragraph();
+		PdfPTable timeTable = new PdfPTable(5);
 		
-		PdfPTable timeTable = new PdfPTable(3);
+		double totalEstimated = 0;
+		double totalActual = 0;
+		double actualToDate = 0;
+		double totalActualToDate = 0;
+		double actualToDatePercent = 0;
+
+		 addEmptyLine(table1, 5);
 		
+		//Add headers to table
 	    PdfPCell tableHeading = new PdfPCell(new Phrase("Time in Phase\n(Minutes)", headingFont));
 	    tableHeading.setHorizontalAlignment(Element.ALIGN_CENTER);
 	    timeTable.addCell(tableHeading);
@@ -124,7 +133,14 @@ public class ProjectSummaryCreator {
 	    timeTable.addCell(tableHeading);
 	    timeTable.setHeaderRows(1);
 	    
+  	  	//Calculate total actual to date
+	    for(int j = 0; j < 7; ++j){ 
+	    	totalActualToDate += actualTime[j];
+	    }
+	    
+	    //Add the data to the table
 	    for(int i = 0; i < 8; ++i){
+	    	//Add Table labels in first column
 	    	switch(i){	    	
 	    	case 0:
 	    		timeTable.addCell("Planning");
@@ -151,19 +167,204 @@ public class ProjectSummaryCreator {
 	    		timeTable.addCell("TOTAL");
 	    		break;
 	    	}
-	    	timeTable.addCell(String.valueOf(estimatedTime[i]));
-	    	timeTable.addCell(String.valueOf(actualTime[i]));
 	    	
+	    	//Add estimated time to table
+	    	String formattedEstimatedTime = String.format("%.2f",estimatedTime[i]);
+	    	timeTable.addCell(formattedEstimatedTime);
+	    	totalEstimated += estimatedTime[i];
 	    	
+	    	//Add actual time to table
+	    	String formattedAcutalTime = String.format("%.2f", actualTime[i]);
+	    	timeTable.addCell(formattedAcutalTime);
+	    	totalActual += actualTime[i];
+	    	
+	    	//Calculate how much time has been spent to date
 	    	for(int totalSoFar = i; totalSoFar >= 0; --totalSoFar){
-	    		//sum up the actual time
+	    		actualToDate += actualTime[i];
 	    	}
 	    	
+	    	//Add To Date to table
+	    	String formattedActualToDate = String.format("%.2f", actualToDate);
+	    	timeTable.addCell(formattedActualToDate);
+	    	
+	    	//Calculate %To Date
+		    actualToDatePercent = (actualToDate/totalActualToDate)*100;
+		    
+		    //Add %To Date to table
+		    String formattedActualToDatePercent = String.format("%.2f", actualToDatePercent);
+		    timeTable.addCell(formattedActualToDatePercent);
 	    	
 	    }
 	    
 	    table1.add(timeTable);
+	}
+	
+	/**
+	 * 
+	 * @param actualDefects array of defects injected
+	 * 
+	 * Creates the defect table with the Estimated column starred out (***) and
+	 * with the Actual Defects Injected, the To Date Defects Injected, and the
+	 * %To Date Defects Injected
+	 */
+	private static void createDefectTable(int [] actualDefects){
+		Paragraph table2 = new Paragraph();
+		PdfPTable defectTable = new PdfPTable(5);
 		
+		double totalActual = 0;
+		double actualToDate = 0;
+		double totalActualToDate = 0;
+		double actualToDatePercent = 0;
+		
+		addEmptyLine(table2, 5);
+		
+		//Add headers to table
+	    PdfPCell tableHeading = new PdfPCell(new Phrase("Defects Injected", headingFont));
+	    tableHeading.setHorizontalAlignment(Element.ALIGN_CENTER);
+	    defectTable.addCell(tableHeading);
+	    
+	    tableHeading = new PdfPCell(new Phrase("Estimated", headingFont));
+	    tableHeading.setHorizontalAlignment(Element.ALIGN_CENTER);
+	    defectTable.addCell(tableHeading);
+	    
+	    tableHeading = new PdfPCell(new Phrase("Actual", headingFont));
+	    tableHeading.setHorizontalAlignment(Element.ALIGN_CENTER);
+	    defectTable.addCell(tableHeading);
+	    
+	    tableHeading = new PdfPCell(new Phrase("To Date", headingFont));
+	    tableHeading.setHorizontalAlignment(Element.ALIGN_CENTER);
+	    defectTable.addCell(tableHeading);
+	    
+	    tableHeading = new PdfPCell(new Phrase("% To Date", headingFont));
+	    tableHeading.setHorizontalAlignment(Element.ALIGN_CENTER);
+	    defectTable.addCell(tableHeading);
+	    defectTable.setHeaderRows(1);
+	    
+  	  	//Calculate total actual to date
+	    for(int j = 0; j < 7; ++j){ 
+	    	totalActualToDate += actualDefects[j];
+	    }
+	    
+	    //Add the data to the table
+	    for(int i = 0; i < 8; ++i){
+	    	//Add Table labels in first column
+	    	switch(i){	    	
+	    	case 0:
+	    		defectTable.addCell("Planning");
+	    		break;
+	    	case 1:
+	    		defectTable.addCell("Design");
+	    		break;
+	    	case 2:
+	    		defectTable.addCell("Code");
+	    		break;
+	    	case 3:
+	    		defectTable.addCell("Code Review");
+	    		break;
+	    	case 4:
+	    		defectTable.addCell("Compile");
+	    		break;
+	    	case 5:
+	    		defectTable.addCell("Test");
+	    		break;
+	    	case 6:
+	    		defectTable.addCell("Postmortem");
+	    		break;
+	    	case 7:
+	    		defectTable.addCell("TOTAL");
+	    		break;
+	    	}
+	    	
+	    	//Add estimated defects to table - at this point, they are starred out (***)
+	    	defectTable.addCell("***");
+	    	
+	    	
+	    	//Add actual time to table
+	    	String formattedAcutalTime = String.format("%.2f", actualDefects[i]);
+	    	defectTable.addCell(formattedAcutalTime);
+	    	totalActual += actualDefects[i];
+	    	
+	    	//Calculate how much time has been spent to date
+	    	for(int totalSoFar = i; totalSoFar >= 0; --totalSoFar){
+	    		actualToDate += actualDefects[i];
+	    	}
+	    	
+	    	//Add To Date to table
+	    	String formattedActualToDate = String.format("%.2f", actualToDate);
+	    	defectTable.addCell(formattedActualToDate);
+	    	
+	    	//Calculate %To Date
+		    actualToDatePercent = (actualToDate/totalActualToDate)*100;
+		    
+		    //Add %To Date to table
+		    String formattedActualToDatePercent = String.format("%.2f", actualToDatePercent);
+		    defectTable.addCell(formattedActualToDatePercent);
+		    
+	    }
+	    
+	    table2.add(defectTable);
+	}
+	
+	/**
+	 * 
+	 * @param estimatedTotal the estimated total program size (LOC), 
+	 * lines of code per hour (LOC/hour), and defects per thousand lines of code
+	 * (KLOC)
+	 * @param actualTotal the actual total program size (LOC), 
+	 * lines of code per hour (LOC/hour), and defects per thousand lines of code
+	 * (KLOC)
+	 * 
+	 * Creates a table summarizing the program size, the lines of code per hour,
+	 * the number of defects per thousand lines of code, and those totals to date.
+	 * To Date is starred out currently (***)
+	 */
+	
+	private static void createSummaryTable(double[] estimatedTotal, double[] actualTotal){
+		Paragraph table3 = new Paragraph();
+		PdfPTable summaryTable = new PdfPTable(4);
+		
+		addEmptyLine(table3, 7);
+		
+		//Add headers to table
+	    PdfPCell tableHeading = new PdfPCell(new Phrase("")); //First row, first column is blank
+	    summaryTable.addCell(tableHeading);
+	    
+	    tableHeading = new PdfPCell(new Phrase("Estimated", headingFont));
+	    tableHeading.setHorizontalAlignment(Element.ALIGN_CENTER);
+	    summaryTable.addCell(tableHeading);
+	    
+	    tableHeading = new PdfPCell(new Phrase("Actual", headingFont));
+	    tableHeading.setHorizontalAlignment(Element.ALIGN_CENTER);
+	    summaryTable.addCell(tableHeading);
+	    
+	    tableHeading = new PdfPCell(new Phrase("To Date", headingFont));
+	    tableHeading.setHorizontalAlignment(Element.ALIGN_CENTER);
+	    summaryTable.addCell(tableHeading);
+	    
+	    //Add data to the table
+	    for(int i = 0; i < 3; ++i){
+	    	switch(i){
+	    	case 0:
+	    		summaryTable.addCell("Program Size (LOC)");
+	    		break;
+	    	case 1:
+	    		summaryTable.addCell("LOC/Hour");
+	    		break;
+	    	case 2:
+	    		summaryTable.addCell("Defect/KLOC");
+	    		break;
+	    	}
+	    	
+	    	String formattedEstimatedTotal = String.format(".2f", estimatedTotal[i]);
+	    	summaryTable.addCell(formattedEstimatedTotal);
+	    	
+	    	String formattedActualTotal = String.format(".2f", actualTotal[i]);
+	    	summaryTable.addCell(formattedActualTotal);
+	    	
+	    	summaryTable.addCell("***");
+	    }
+	    
+	    table3.add(summaryTable);
 	}
 	
 	/**
