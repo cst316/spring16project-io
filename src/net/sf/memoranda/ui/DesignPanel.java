be2944ff043e759de7f44248712f042c8191b05a
jrmicha1 <jrmicha1@asu.edu>
2016-02-29 00:12:20 -0700

    Added background class to help display image on jpanel. Added a few method and hopefully worked out some kinks in the directory paths.

--- src/net/sf/memoranda/ui/DesignPanel.java
+++ src/net/sf/memoranda/ui/DesignPanel.java
@@ -1,6 +1,8 @@
 package net.sf.memoranda.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
@@ -12,17 +14,20 @@ import java.util.ArrayList;
 
 import javax.swing.JPanel;
 import javax.imageio.ImageIO;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JList;
 
 public class DesignPanel extends JPanel implements FocusListener{
+	
 	BorderLayout borderLayout1 = new BorderLayout();
 	DailyItemsPanel parentPanel = null;
 	
 	JPanel backPanel = new JPanel();
 	JPanel designView = new JPanel();
 	JPanel panel_1 = new JPanel();
+	JPanel viewPanel = new JPanel();
 	
 	JButton btnImportDesign = new JButton("Import Design");
 	JList<String> fileList = new JList<String>(getFileList());
@@ -33,8 +38,9 @@ public class DesignPanel extends JPanel implements FocusListener{
 	 * may need to write xml importer for such info if I can't pull from super.
 	 */
 	
-	int pID;
-	String projectName;
+	//not sure how to get these right now
+	private int pID;
+	private String projectName;
 	
 	public DesignPanel(DailyItemsPanel _parentPanel) {
 		setLayout(null);
@@ -65,6 +71,10 @@ public class DesignPanel extends JPanel implements FocusListener{
 		add(panel_1);
 		
 		panel_1.add(fileList);
+		
+		
+		viewPanel.setBounds(101, 0, 339, 289);
+		add(viewPanel);
         
 		btnImportDesign.addActionListener(new java.awt.event.ActionListener() {
 	        public void actionPerformed(ActionEvent e) {
@@ -91,19 +101,19 @@ public class DesignPanel extends JPanel implements FocusListener{
 				System.out.println("Selected file: " + selectedFile.getName());
 				
 				//pathways need to be updated to be relative. left here for testing purposes
-				if(!Files.exists(Paths.get("C:\\Users\\Joe\\Documents\\memorandaFiles")))
+				if(!Files.exists(Paths.get("projects\\"+projectName+"\\images\\design")))
 				{
-					File file = new File("C:\\Users\\Joe\\Documents\\memorandaFiles");
+					File file = new File("projects\\"+projectName+"\\images\\design");
 					file.mkdirs();
 				}
 			
 				if (selectedFile.getName().contains(".png"))
 				{
-					ImageIO.write(image, "png", new File("C:\\Users\\Joe\\Documents\\memorandaFiles\\out.png"));
+					ImageIO.write(image, "png", new File("projects\\"+projectName+"\\images\\design"));
 				}
 				else if (selectedFile.getName().contains(".jpg"))
 				{
-					ImageIO.write(image, "jpg", new File("C:\\Users\\Joe\\Documents\\memorandaFiles\\out.jpg"));
+					ImageIO.write(image, "jpg", new File("projects\\"+projectName+"\\images\\design"));
 				}
 			}
 				
@@ -116,29 +126,40 @@ public class DesignPanel extends JPanel implements FocusListener{
 	
 	/*
 	 * @return imports file names from folder, returns as string array
-	 * Note: update path once tacked down.
+	 * Note: update path once tacked down. Should I also buffer the images in the file as well?
 	 */
-	public String[] getFileList()
+	public DefaultListModel<String> getFileList()
 	{
-		ArrayList<String> fileArrList = new ArrayList<String>();
-		String path = "C:\\Users\\Joe\\Documents\\memorandaFiles";
+		DefaultListModel<String> listModel = new DefaultListModel<String>();
+		String path = "projects\\"+projectName+"\\images\\design";
 		
 		File data = new File(path);
 		File[] files = data.listFiles();
 		for(int i = 0; i < files.length; ++i)
 		{
-			fileArrList.add(files[i].toString());
+			listModel.addElement(files[i].toString());
 		}
 		
-		return (String[]) fileArrList.toArray();
+		return listModel;
 	}
 	@Override
-	public void focusGained(FocusEvent arg0) {
+	public void focusGained(FocusEvent event) {
 		//display picture in main panel
+		
+		try{
+			viewPanel.add(new BackGround(event.getSource().toString(), viewPanel.getWidth(), viewPanel.getHeight()));	
+		}catch(RuntimeException ex){
+			ex.getMessage();
+		}catch(Exception e){
+			e.getMessage();
+		}
+		viewPanel.invalidate();
+		viewPanel.validate();
+		viewPanel.repaint();
 		
 	}
 	@Override
-	public void focusLost(FocusEvent arg0) {
+	public void focusLost(FocusEvent event) {
 		// TODO Auto-generated method stub
 		
 	}
