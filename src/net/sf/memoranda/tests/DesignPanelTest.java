2aae6fca05ce71eb593f164480716af9fe9b667d
jrmicha1 <jrmicha1@asu.edu>
2016-03-01 16:38:56 -0700

    test design panel

---
+++ src/net/sf/memoranda/tests/DesignPanelTest.java
@@ -0,0 +1,14 @@
+package net.sf.memoranda.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DesignPanelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
