673b210a35065e2f0376ca97d74692251eb9399f
Julie Teplik <jteplik@asu.edu>
2016-03-01 13:13:33 -0700

    Started work on JUnit test for ProjectSummaryCreator

---
+++ src/net/sf/memoranda/tests/ProjectSummaryCreatorTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package net.sf.memoranda.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Julie
+ * Tests the class ProjectSummaryCreator
+ */
+public class ProjectSummaryCreatorTest {
+	private String author;
+	private String programName;
+	private String date;
+	private double[] estimatedTime;
+	private double[] actualTime;
+	private int[] actualDefects;
+	private double[] estimatedTotal; 
+	private double[] actualTotal;
+	/**
+	 * @throws java.lang.Exception
+	 * Creates a test PDF
+	 */
+	@Before
+	public void setUp() throws Exception {
+		author = "Bob Smith";
+		programName = "Test Program";
+		date = "Febuary 29 2016";
+		estimatedTime = new double[] {5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 35.0};
+		actualTime = new double[] {5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 35.0};
+		actualDefects = new int[] {1, 1, 1, 1, 1, 1, 1, 7};
+		estimatedTotal = new double[] {50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 350.0};
+		actualTotal = new double[] {50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 350.0};
+	}
+
+	/**
+	 * Test method for {@link net.sf.memoranda.psp.ProjectSummaryCreator#createFileSummary(java.lang.String, java.lang.String, java.lang.String, double[], double[], int[], double[], double[])}.
+	 */
+	@Test
+	public void testCreateFileSummary() {
+		fail("Not yet implemented");
+	}
+
+}
