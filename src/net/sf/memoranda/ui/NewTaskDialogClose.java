e46e4ea0811682f122962c0cf9413653202ae31c
Jeff <jospina@asu.edu>
2016-03-02 11:06:34 -0700

    Fixed oversight with jbuttons not calling task/closetask dialogs.

--- src/net/sf/memoranda/ui/NewTaskDialogClose.java
+++ src/net/sf/memoranda/ui/NewTaskDialogClose.java
@@ -23,7 +23,7 @@ public class NewTaskDialogClose extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void NewScreen() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
