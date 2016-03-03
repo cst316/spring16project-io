2e046ae99a4a434791bbde52d7734c1ca408f451
jrmicha1 <jrmicha1@asu.edu>
2016-03-02 23:58:02 -0700

    resized design panel layout slightly. file list now shows when files are added... but buggy. image still not displaying... work in progress.

--- src/net/sf/memoranda/ui/PSP_DesignPanel.java
+++ src/net/sf/memoranda/ui/PSP_DesignPanel.java
@@ -1,8 +1,7 @@
 package net.sf.memoranda.ui;
 
 import java.awt.BorderLayout;
-import java.awt.Image;
-import java.awt.Toolkit;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
@@ -10,7 +9,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
@@ -18,8 +16,10 @@ import net.sf.memoranda.util.Util;
 
 import javax.imageio.ImageIO;
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.JList;
 
 public class PSP_DesignPanel extends JPanel implements FocusListener {
@@ -46,26 +46,19 @@ public class PSP_DesignPanel extends JPanel implements FocusListener {
 	JPanel viewPanel = new JPanel();
 	
 	JButton btnImportDesign = new JButton("Import Design");
-	//JList<String> fileList;
+	JList<String> fileList;
 	
-	
-	/*
-	 * note: need to know current project name and/or pID, 
-	 * may need to write xml importer for such info if I can't pull from super.
-	 */
-	
-	//not sure how to get these right now, probably don't need the pid
-	private int pID;
-	private String projectName; // same as pid
+	private String path;
+	private int pID; // no setter made for pID yet. want to minimize ways to change this value.
 	private static PSP_Panel pspForm;
-	
+
 	public PSP_DesignPanel(PSP_Panel psp){
 		
 		PSP_DesignPanel.pspForm = psp;
-		pID = pspForm.pspI.getpId();
+		this.pID = pspForm.pspI.getpId();
+		setPath((Integer.toString(pID)));
+		
 		try {
-            //parentPanel = _parentPanel;
-            //pID = parentPanel;
             jbInit();
         }
         catch (Exception ex) {
@@ -77,8 +70,6 @@ public class PSP_DesignPanel extends JPanel implements FocusListener {
 		
 		
 		try {
-            //parentPanel = _parentPanel;
-            //pID = parentPanel;
             jbInit();
         }
         catch (Exception ex) {
@@ -88,24 +79,35 @@ public class PSP_DesignPanel extends JPanel implements FocusListener {
     }
 	private void jbInit() throws Exception 
 	{
+		try{
+			fileList = new JList<String>(getFileList());
+		}catch (Exception e){
+			e.getMessage();
+		}
 		
-		//fileList = new JList<String>(getFileList());
-		//fileList.addFocusListener(this);
 		setLayout(null);
-		backPanel.setBounds(0, 0, 91, 44);
+		backPanel.setBounds(0, 0, 139, 44);
 		add(backPanel);
 		backPanel.setLayout(null);
 		
-		btnImportDesign.setBounds(0, 11, 89, 23);
+		btnImportDesign.setBounds(10, 11, 119, 23);
 		backPanel.add(btnImportDesign);
 		
 		
-		listPanel.setBounds(0, 55, 91, 234);
+		listPanel.setBounds(0, 55, 139, 234);
 		add(listPanel);
 		
-		//listPanel.add(fileList);
+		if (fileList != null){
+			listPanel.add(fileList);
+			fileList.addFocusListener(this);
+			listPanel.invalidate();
+			listPanel.validate();
+			listPanel.repaint();
+		}else{
+			Util.debug("file list is empty");
+		}
 		
-		viewPanel.setBounds(101, 0, 339, 289);
+		viewPanel.setBounds(149, 0, 291, 289);
 		add(viewPanel);
         
 		btnImportDesign.addActionListener(new java.awt.event.ActionListener() {
@@ -120,7 +122,6 @@ public class PSP_DesignPanel extends JPanel implements FocusListener {
 	
 	public void btnImportDesign_actionPerformed(ActionEvent e)
 	{//import file  and save in memoranda directory
-		Util.debug("enter method.");
 		BufferedImage image = null; 
 		try
 		{
@@ -135,27 +136,25 @@ public class PSP_DesignPanel extends JPanel implements FocusListener {
 				System.out.println("Selected file: " + selectedFile.getName());
 				
 				//pathways need to be updated to be relative.
-				if(!Files.exists(Paths.get(System.getProperty("user.home") + File.separator + ".memoranda" + File.separator + ".proj" + File.separator + 
-						 "." + pID +  File.separator + ".images" + File.separator 
-						+ ".design")))
+				if(!Files.exists(Paths.get(getPath())))
 				{
-					File file = new File(System.getProperty("user.home") + File.separator + ".memoranda" + File.separator + ".proj" + File.separator + 
-							 "." + pID +  File.separator + ".images" + File.separator 
-								+ ".design");
+					File file = new File(getPath());
 					file.mkdirs();
 				}
 			
 				if (selectedFile.getName().contains(".png"))
 				{
-					ImageIO.write(image, "png", new File(System.getProperty("user.home") + File.separator + ".memoranda" + File.separator + ".proj" + File.separator + 
-							 "." + pID +  File.separator + ".images" + File.separator 
-								+ ".design" + File.separator + selectedFile.getName()));
+					ImageIO.write(image, "png", new File(getPath() + File.separator + selectedFile.getName()));
 				}
 				else if (selectedFile.getName().contains(".jpg"))
 				{
-					ImageIO.write(image, "jpg", new File(System.getProperty("user.home") + File.separator + ".memoranda" + File.separator + ".proj" + File.separator + 
-							 "." + pID +  File.separator + ".images" + File.separator 
-								+ ".design" + File.separator + selectedFile.getName()));
+					ImageIO.write(image, "jpg", new File(getPath() + File.separator + selectedFile.getName()));
+				}else if (selectedFile.getName().contains(".img"))
+				{
+					ImageIO.write(image, "img", new File(getPath() + File.separator + selectedFile.getName()));
+				} else if (selectedFile.getName().contains(".tif"))
+				{
+					ImageIO.write(image, "tif", new File(getPath() + File.separator + selectedFile.getName()));
 				}
 			}
 				
@@ -165,10 +164,15 @@ public class PSP_DesignPanel extends JPanel implements FocusListener {
 			throw new RuntimeException("Error saving image, Check image type");
 		}
 		//reinitialize filelist after import is run
-		//fileList = new JList<String>(getFileList());
-		listPanel.invalidate();
-		listPanel.validate();
-		listPanel.repaint();
+		try{
+			fileList = new JList<String>(getFileList());
+			fileList.addFocusListener(this);
+			listPanel.invalidate();
+			listPanel.validate();
+			listPanel.repaint();
+		}catch (Exception ex){
+			ex.getMessage();
+		}
 	}
 	
 	/*
@@ -178,14 +182,12 @@ public class PSP_DesignPanel extends JPanel implements FocusListener {
 	public DefaultListModel<String> getFileList()
 	{
 		DefaultListModel<String> listModel = new DefaultListModel<String>();
-		String path = "projects" + File.separator +projectName + 
-				File.separator + "images" + File.separator + "design";
-		//only search for picture files ... png, jpg, etc...
-		File data = new File(path);
+		//need to narrow its search to display only picture files types like png, jpg, img etc...
+		File data = new File(getPath());
 		File[] files = data.listFiles();
 		for(int i = 0; i < files.length; ++i)
 		{
-			listModel.addElement(files[i].toString());
+			listModel.addElement(files[i].getName());
 		}
 		
 		return listModel;
@@ -193,24 +195,45 @@ public class PSP_DesignPanel extends JPanel implements FocusListener {
 	@Override
 	public void focusGained(FocusEvent event) {
 		//display picture in main panel
-		
+		Util.debug("focus gained");
 		try{
-			viewPanel.add(new BackGround(event.getSource().toString(), 
-					viewPanel.getWidth(), viewPanel.getHeight()));	
+			ImageIcon icon = new ImageIcon(getPath() + File.separator + event.getSource().toString());
+			JLabel imgLabel = new JLabel();
+			imgLabel.setIcon(icon);
+			viewPanel.add(imgLabel);
+/*			viewPanel.setBackground(new BackGround(event.getSource().toString(), 
+					viewPanel.getWidth(), viewPanel.getHeight()));
+*/			viewPanel.setAutoscrolls(true);
 		}catch(RuntimeException ex){
-			ex.getMessage();
+			Util.debug(ex.getMessage());
 		}catch(Exception e){
-			e.getMessage();
+			Util.debug(e.getMessage());
 		}
+		Util.debug("background added to panel... revalidating");
 		viewPanel.invalidate();
 		viewPanel.validate();
 		viewPanel.repaint();
-		
+		Util.debug("revalidating done");
 	}
 	@Override
 	public void focusLost(FocusEvent event) {
 		// TODO Auto-generated method stub
-		
+		//do nothing
+	}
+	
+	public boolean setPath(String pId){
+		this.path = System.getProperty("user.home") + File.separator + 
+		".memoranda" + File.separator + ".proj" + File.separator + 
+		 "." + pID +  File.separator + ".images" + File.separator 
+			+ ".design";
+		return true;
+	}
+	public String getPath(){
+		return this.path;
+	}
+	
+	public int getPID(){
+		return this.pID;
 	}
 }
 
