77e4b095bb29b2ded25b78d2d31a1616be3872d6
jrmicha1 <jrmicha1@asu.edu>
2016-03-15 16:58:50 -0700

    Built out most of Testing interface, need to begin filling out TestingImpl. Basic framework is set up.

--- src/net/sf/memoranda/psp/Testing.java
+++ src/net/sf/memoranda/psp/Testing.java
@@ -1,12 +1,44 @@
 package net.sf.memoranda.psp;
 
+import java.io.File;
+import java.util.Date;
+
 public interface Testing extends Psp {
 
-	//pid accessor
+	
+	public boolean loadTestData(File file); //used in constructor elsewhere if needed
+	public boolean saveTestData(); //creates and saves file using current data
+	
+	// setters and getters
 	public int getPID();
 	
-	//file name accessor
 	public String getFileName();
 	
+	public String getUserName();
 	
+	public boolean setUserName(String name);
+	
+	public int getDefectNum();
+	
+	public boolean setDefectNum(int num);
+	
+	public Date getDate();
+	
+	public boolean setDate(Date date);
+	
+	public String getType();
+	
+	public boolean setType(String type);
+	
+	public String getInjectionPhase();
+	
+	public boolean setInjectionSite(String injectPhase);
+	
+	public String getRemovalPhase();
+	
+	public boolean setRemovalPhase(String removalPhase);
+	
+	public String getFixDetails();
+	
+	public boolean setFixDetails(String fix);
 }
--- src/net/sf/memoranda/psp/TestingImpl.java
+++ src/net/sf/memoranda/psp/TestingImpl.java
@@ -1,11 +1,24 @@
 package net.sf.memoranda.psp;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.Date;
 
 import net.sf.memoranda.date.CurrentDate;
 
 public class TestingImpl implements Testing {
+	
+	
+	
+	public TestingImpl(){
+		
+	}
+	
+	public TestingImpl(File file){
+		super();
+		loadTestData(file);
+	}
 
 	@Override
 	public CurrentDate getStDate() {
@@ -67,6 +80,7 @@ public class TestingImpl implements Testing {
 		
 	}
 
+
 	@Override
 	public int getPID() {
 		// TODO Auto-generated method stub
@@ -77,6 +91,102 @@ public class TestingImpl implements Testing {
 	public String getFileName() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public String getUserName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean setUserName(String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getDefectNum() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean setDefectNum(int num) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Date getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean setDate(Date date) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean setType(String type) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getInjectionPhase() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean setInjectionSite(String injectPhase) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getRemovalPhase() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean setRemovalPhase(String removalPhase) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getFixDetails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean setFixDetails(String fix) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean loadTestData(File file) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean saveTestData() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
