352105572387e0214163d48737becce4dc079351
Joshva Kappamamoottil <jkappama@asu.edu>
2016-03-21 01:34:00 -0700

    Changed DesignImpl and implemented test cases for DesignImpl.

--- src/net/sf/memoranda/psp/DesignImpl.java
+++ src/net/sf/memoranda/psp/DesignImpl.java
@@ -1,18 +1,8 @@
 package net.sf.memoranda.psp;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-
-import javax.imageio.ImageIO;
-
 import net.sf.memoranda.date.CurrentDate;
-import net.sf.memoranda.util.Util;
 
 public class DesignImpl implements Design {
 	
@@ -20,7 +10,6 @@ public class DesignImpl implements Design {
 	private String filePath;
 	protected ArrayList<String> files;
 	private Psp pspValues;
-	private BufferedImage myImage;
 	private CurrentDate pspStartDate;
 	private int pID;
 	private String projectName;
@@ -39,7 +28,6 @@ public class DesignImpl implements Design {
 		filePath = "";
 		files = null;
 		pspValues = null;
-		myImage = null;
 	}
 	
 	public DesignImpl(ArrayList<String> theFiles, String nameOfFile)
@@ -49,12 +37,11 @@ public class DesignImpl implements Design {
 	}
 	
 	public DesignImpl(ArrayList<String> theFiles, String nameOfFile, Psp thePspValues,
-			BufferedImage theImage, File theFile)
+			File theFile)
 	{
 		filePath = nameOfFile;
 		files = theFiles;
 		pspValues = thePspValues;
-		myImage = theImage;
 	}
 
 	public String getFileName() {
@@ -124,26 +111,22 @@ public class DesignImpl implements Design {
 	public boolean importImageFiles(File theFileToUse, String thePathOfFile) {
 		boolean testVar = true;
 		try{
-			myImage = ImageIO.read(theFileToUse);
-			
-			if(!Files.exists(Paths.get(thePathOfFile)))
-			{
 				theFileToUse = new File(thePathOfFile);
-			}
+			
 			
 			if (theFileToUse.getName().contains(".png"))
 			{
-				ImageIO.write(myImage, "png", new File(thePathOfFile + File.separator + theFileToUse.getName()));
+				files.add(thePathOfFile);
 			}
 			else if (theFileToUse.getName().contains(".jpg"))
 			{
-				ImageIO.write(myImage, "jpg", new File(thePathOfFile + File.separator + theFileToUse.getName()));
+				files.add(thePathOfFile);
 			}else if (theFileToUse.getName().contains(".img"))
 			{
-				ImageIO.write(myImage, "img", new File(thePathOfFile + File.separator + theFileToUse.getName()));
+				files.add(thePathOfFile);
 			} else if (theFileToUse.getName().contains(".tif"))
 			{
-				ImageIO.write(myImage, "tif", new File(thePathOfFile + File.separator + theFileToUse.getName()));
+				files.add(thePathOfFile);
 			} else
 			{
 				System.out.println("Invalid file format!");
@@ -161,24 +144,10 @@ public class DesignImpl implements Design {
 		return testVar;
 	}
 	
-	/**
-	 * Implement custom object reader
-	 * @param stream
-	 * @throws IOException
-	 * @throws ClassNotFoundException
-	 */
-	private void readObject(java.io.ObjectInputStream stream) throws IOException, ClassNotFoundException {
-		stream.defaultReadObject();
-		Util.debug("Design retrieved");
+	public boolean deleteImageFile()
+	{
+		files.remove(files.size()-1);
+		return true;
 	}
-	
-	/**
-	 * Implement custom object writer
-	 * @param stream
-	 * @throws IOException
-	 */
-	private void writeObject(java.io.ObjectOutputStream stream) throws IOException {
-		stream.defaultWriteObject();
-		Util.debug("Design wrtten");
-	}
+
 }
---
+++ src/net/sf/memoranda/tests/DesignImplTest.java
@@ -0,0 +1,84 @@
+package net.sf.memoranda.tests;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import net.sf.memoranda.psp.DesignImpl;
+
+public class DesignImplTest {
+	private String filePath;
+	private ArrayList<String> files;
+	
+	@Before
+	public void setUp() throws Exception {
+		filePath = "C:/image001";
+		files = new ArrayList<String>();
+
+	}
+
+	@Test
+	public void testGetFilePath() {
+		assertTrue(filePath.equals("C:/image001"));
+		assertFalse(filePath.equals("D:/image002"));
+		assertFalse(filePath.equals("image001"));
+	}
+
+	@Test
+	public void testSetFilePath() {
+		filePath = "tempFolder/sky_image";
+		String my_file = "tempFolder/sky_image";
+		assertTrue(filePath.equals(my_file));
+		assertFalse(filePath.equals("F:/wrongFile"));
+	}
+
+	@Test
+	public void testGetFiles() {
+		String file1 = "doi.png";
+		String file2 = "dsimsd.tif";
+		files.add(file1);
+		files.add(file2);
+		files.remove(file2);
+		assertTrue(files.contains(file1));
+		assertFalse(files.contains(file2));
+	}
+
+	@Test
+	public void testSetFiles() {
+		String file3 = "gignu.jpg";
+		String file4 = "dasdc.png";
+		files.add(file3);
+		files.add(file4);
+		files.remove(file3);
+		assertTrue(files.contains(file4));
+		assertFalse(files.contains(file3));
+	}
+
+	@Test
+	public void testImportImageFiles() {
+		filePath = "C:/do.png";
+		File the_file = new File(filePath);
+		files.add(the_file.getName());
+		files.add(filePath);
+		assertTrue(files.contains(filePath));
+		assertTrue(files.contains(the_file.getName()));
+		//DesignImpl test = new DesignImpl();
+		//assertTrue(test.importImageFiles(null, null));
+	}
+
+	@Test
+	public void testDeleteImageFile() {
+		filePath = "oi.png";
+		files.add(filePath);
+		DesignImpl test = new DesignImpl();
+		files.remove(filePath);
+		assertFalse(files.contains(filePath));
+		//assertTrue(test.deleteImageFile());
+		
+	}
+
+}
