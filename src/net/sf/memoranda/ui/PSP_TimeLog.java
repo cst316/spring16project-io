f5993726726ea7ec2a055bf748802804431cd5c6
Julie Teplik <jteplik@asu.edu>
2016-03-23 13:33:51 -0700

    Turned PSP_TimeLog into a frame instead of a JPanel

--- src/net/sf/memoranda/ui/PSP_TimeLog.java
+++ src/net/sf/memoranda/ui/PSP_TimeLog.java
@@ -3,6 +3,7 @@ package net.sf.memoranda.ui;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -10,15 +11,6 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
-/**
- * PSP Time Log. Used for users to enter the time that they spent on the project.
- * @author Julie
- *
- */
 
 public class PSP_TimeLog extends JFrame {
 
@@ -28,6 +20,8 @@ public class PSP_TimeLog extends JFrame {
 	private JTextField interruptTimeTextField;
 	private JTextField endTimeTextField;
 	private JTextField phaseTextField;
+	private JButton bttnDone;
+	private JButton bttnMoreEntries;
 
 	/**
 	 * Launch the application.
@@ -59,68 +53,62 @@ public class PSP_TimeLog extends JFrame {
 		JLabel lblTimeLogEntries = new JLabel("Time Log Entries");
 		lblTimeLogEntries.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTimeLogEntries.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblTimeLogEntries.setBounds(139, 11, 157, 19);
+		lblTimeLogEntries.setBounds(94, 11, 229, 26);
 		contentPane.add(lblTimeLogEntries);
 		
 		JLabel lblDate = new JLabel("Date");
-		lblDate.setBounds(10, 55, 46, 14);
+		lblDate.setBounds(17, 61, 46, 14);
 		contentPane.add(lblDate);
 		
-		JLabel lblTimeStarted = new JLabel("Time Started");
-		lblTimeStarted.setBounds(74, 55, 70, 14);
-		contentPane.add(lblTimeStarted);
+		JLabel lblStartTime = new JLabel("Start Time");
+		lblStartTime.setBounds(73, 61, 67, 14);
+		contentPane.add(lblStartTime);
 		
 		JLabel lblInterruptTime = new JLabel("Interrupt Time");
-		lblInterruptTime.setBounds(154, 55, 88, 14);
+		lblInterruptTime.setBounds(150, 61, 86, 14);
 		contentPane.add(lblInterruptTime);
 		
 		JLabel lblEndTime = new JLabel("End Time");
-		lblEndTime.setBounds(234, 55, 62, 14);
+		lblEndTime.setBounds(265, 61, 46, 14);
 		contentPane.add(lblEndTime);
 		
 		JLabel lblPhase = new JLabel("Phase");
-		lblPhase.setBounds(319, 55, 46, 14);
+		lblPhase.setBounds(344, 61, 46, 14);
 		contentPane.add(lblPhase);
 		
 		dateTextField = new JTextField();
-		dateTextField.setBounds(0, 88, 62, 20);
+		dateTextField.setBounds(10, 86, 67, 20);
 		contentPane.add(dateTextField);
 		dateTextField.setColumns(10);
 		
 		startTimeTextField = new JTextField();
-		startTimeTextField.setBounds(74, 88, 77, 20);
+		startTimeTextField.setBounds(83, 86, 67, 20);
 		contentPane.add(startTimeTextField);
 		startTimeTextField.setColumns(10);
 		
 		interruptTimeTextField = new JTextField();
-		interruptTimeTextField.setBounds(154, 88, 70, 20);
+		interruptTimeTextField.setBounds(160, 86, 86, 20);
 		contentPane.add(interruptTimeTextField);
 		interruptTimeTextField.setColumns(10);
 		
 		endTimeTextField = new JTextField();
-		endTimeTextField.setBounds(234, 88, 70, 20);
+		endTimeTextField.setBounds(259, 86, 86, 20);
 		contentPane.add(endTimeTextField);
 		endTimeTextField.setColumns(10);
 		
 		phaseTextField = new JTextField();
-		phaseTextField.setBounds(319, 88, 105, 20);
+		phaseTextField.setBounds(338, 86, 86, 20);
 		contentPane.add(phaseTextField);
 		phaseTextField.setColumns(10);
 		
-		JButton btnSaveButton = new JButton("Save");
-		btnSaveButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnSaveButton.setBounds(215, 202, 89, 23);
-		contentPane.add(btnSaveButton);
+		bttnDone = new JButton();
+		bttnDone.setText("Done");
+		bttnDone.setBounds(237,193,86,20);
+		contentPane.add(bttnDone);
 		
-		JButton btnMoreEntries = new JButton("More Entries");
-		btnMoreEntries.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnMoreEntries.setBounds(74, 202, 105, 23);
-		contentPane.add(btnMoreEntries);
+		bttnMoreEntries = new JButton();
+		bttnMoreEntries.setText("More Entries");
+		bttnMoreEntries.setBounds(73,193,112,20);
+		contentPane.add(bttnMoreEntries);
 	}
 }
