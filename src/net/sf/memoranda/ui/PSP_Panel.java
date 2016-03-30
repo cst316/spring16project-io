255bc7c0b5c82b50d0fd5e9500115af423abf0c6
Cephas Armstrong-Mensah <carmstr7@asu.edu>
2016-03-30 12:41:13 -0700

    Working on some more opening and closing tasks for PSP wrap up

--- src/net/sf/memoranda/ui/PSP_Panel.java
+++ src/net/sf/memoranda/ui/PSP_Panel.java
@@ -26,6 +26,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.awt.SystemColor;
@@ -166,76 +167,26 @@ public class PSP_Panel extends JPanel{
 	 * @param event - Used to know which action to perform
 	 */
 	private void project_MouseEvent (String event) {
-		ObjectInputStream ois;
-		File fs;
-		
-		try {
-			if (event.equals("NEW PROJECT")) {
-				App.getFrame().setEnabled(false);
-				toolBar.setVisible (false);
-				setEnabledFlag (lblOpenProject, getIsNeeded());
-				(new PSP_NPWizardFrame(this)).setVisible(true);			
-			} else if (event.equals("OPEN PROJECT")) {
-				if (currentView != null) {
-					Util.debug("CURRENT VIEW: " + currentView.getName());
-				}
-				openFileDialog();			
-			} else if (event.equals("PLANNING")) {
-				// check code here to handle between switching between items or opening file
-				fs = new File (System.getProperty("user.home") +  File.separator + ".memoranda" + 
-					File.separator + ".proj" + File.separator + '.' + pspI.getpId() + 
-					File.separator + '.' + pspI.getpId() + "_planning");
-				ois = new ObjectInputStream (new FileInputStream (fs));
-				plan = (Planning) ois.readObject();									
-				addJPanel (new PSP_PlanningPanel (plan));		
-				ois.close();
-				 		
-			} else if (event.equals("DESIGN")) {
-				addJPanel(new PSP_DesignPanel(this));
-				System.out.println("Yeah Design");
-			} else if (event.equals("DEFECT")) {
-				fs = new File (System.getProperty("user.home") +  File.separator + ".memoranda" + 
-					File.separator + ".proj" + File.separator + '.' + pspI.getpId() + 
-					File.separator + '.' + pspI.getpId() + "_defect");
-				if (fs.exists()) {
-					ois = new ObjectInputStream (new FileInputStream (fs));
-					defect = (Defect) ois.readObject();
-					addJPanel (new PSP_DefectPanel (defect));		
-					ois.close();
-				} else {
-					addJPanel (new PSP_DefectPanel ());
-				}
-			} else if (event.equals("TIMELOG")) {
-				fs = new File (System.getProperty("user.home") +  File.separator + ".memoranda" + 
-					File.separator + ".proj" + File.separator + '.' + pspI.getpId() + 
-					File.separator + '.' + pspI.getpId() + "_timelog");
-				if (fs.exists()) {
-					ois = new ObjectInputStream (new FileInputStream (fs));
-					timelog = (TimeLog) ois.readObject();
-					addJPanel (new PSP_TimeLog (timelog));	
-					ois.close();
-				} else {
-					addJPanel(new PSP_TimeLog ());							
-				}
-			} else if (event.equals("DEVELOPMENT")) {
-				fs = new File (System.getProperty("user.home") +  File.separator + ".memoranda" + 
-						File.separator + ".proj" + File.separator + '.' + pspI.getpId() + 
-						File.separator + '.' + pspI.getpId() + "_development");
-				if (fs.exists()) {
-					ois = new ObjectInputStream (new FileInputStream (fs));
-					dev = (Development) ois.readObject();
-					addJPanel (new PSP_NewTaskTable (dev));		
-					ois.close();
-				} else {
-					addJPanel (new PSP_NewTaskTable ());
-				}
-			}	else if (event.equals("PSP")){
-				PSP_Details details = new PSP_Details(pspI);					
-				addJPanel (details);
-			}
-		}	catch (IOException | ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (event.equals("NEW PROJECT")) {
+			App.getFrame().setEnabled(false);
+			toolBar.setVisible (false);
+			setEnabledFlag (lblOpenProject, getIsNeeded());
+			(new PSP_NPWizardFrame(this)).setVisible(true);			
+		} else if (event.equals("OPEN PROJECT")) {
+			openFileDialog();			
+		} else if (event.equals("PLANNING")) {
+			addJPanel (new PSP_PlanningPanel (plan));
+		} else if (event.equals("DESIGN")) {
+			addJPanel(new PSP_DesignPanel(this));
+		} else if (event.equals("DEFECT")) {
+			addJPanel (new PSP_DefectPanel (defect));
+		} else if (event.equals("TIMELOG")) {
+			addJPanel(new PSP_TimeLog (timelog));
+		} else if (event.equals("DEVELOPMENT")) {
+			addJPanel (new PSP_NewTaskTable (dev));
+		} else if (event.equals("PSP")){
+			PSP_Details details = new PSP_Details(pspI);					
+			addJPanel (details);
 		}
 	}
 	
@@ -274,14 +225,18 @@ public class PSP_Panel extends JPanel{
 				File.separator + ".proj" + File.separator + ".pspxFiles"));
 		int returnVal = fc.showOpenDialog(this);
 		
-		ObjectInputStream ois = null;
+		ObjectInputStream ois;
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
 			try {
 				File openThis = fc.getSelectedFile();
 				ois = new ObjectInputStream (new FileInputStream (openThis.getAbsolutePath()));	
 				pspI = (PspImpl) ois.readObject();
+				
+				//Checks to see what other snap ins need to be opened for the opened project
+				openSavedSnapIns (new File (System.getProperty("user.home") +  File.separator + 
+						".memoranda" + File.separator + ".proj" + File.separator + '.' + pspI.getpId()));
 				ois.close();
-
+				
 				if (currentView instanceof PSP_DesignPanel) {
 					project_MouseEvent ("DESIGN");
 				}  else if (currentView instanceof PSP_DefectPanel) {
@@ -289,13 +244,12 @@ public class PSP_Panel extends JPanel{
 				} else if (currentView instanceof PSP_PlanningPanel) {
 					project_MouseEvent ("PLANNING");
 				} else if (currentView instanceof PSP_NewTaskTable) {
-					project_MouseEvent ("DEVELOPMENT");				
-				} /*else if (currentView instanceof PSP_TimeLogPanel) {
-					project_MouseEvent ("PLANNING");				
-				} */else {
+					dev = (Development) ois.readObject();														
+				} else if (currentView instanceof PSP_TimeLog) {
+					project_MouseEvent ("TIMELOG");
+				} else {
 					project_MouseEvent ("PSP");
-				}
-				
+				}				
 				projOpened = true;
 				if (toolBar.getComponentCount() <= 3)
 					setExtraTools ();
@@ -311,6 +265,53 @@ public class PSP_Panel extends JPanel{
 		return projOpened;
 	}
 	
+	private void openSavedSnapIns(File fs) {
+		// TODO Auto-generated method stub
+		File temp;
+		ObjectInputStream ois;
+		
+		try {
+			temp = new File (fs, "." + pspI.getpId() + "_planning");
+			Util.debug("File path: " + temp.getPath());
+			if (temp.exists()) {
+				ois = new ObjectInputStream (new FileInputStream (temp));
+				plan = (Planning) ois.readObject();	
+				ois.close();
+			}
+			
+			temp = new File (fs, "_development");
+			if (temp.exists()) {
+				ois = new ObjectInputStream (new FileInputStream (temp));
+				dev = (Development) ois.readObject();	
+				ois.close();
+			}
+			
+			temp = new File (fs, "_timelog");
+			if (temp.exists()) {
+				ois = new ObjectInputStream (new FileInputStream (temp));
+				timelog = (TimeLog) ois.readObject();
+				ois.close();
+			}
+			
+			temp = new File (fs, "_defect");
+			if (temp.exists()) {
+				ois = new ObjectInputStream (new FileInputStream (temp));
+				defect = (Defect) ois.readObject();	
+				ois.close();
+			}			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
 	public void setExtraTools () {
 		// TODO Auto-generated method stub
 		JLabel lblPSP = new JLabel("Details");
