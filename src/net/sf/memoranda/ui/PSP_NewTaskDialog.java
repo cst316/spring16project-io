415df977a820199170977699ad4f8655b5983a5c
Jeff <jospina@asu.edu>
2016-03-31 12:46:25 -0700

    exiting dialogs was causing main program to close, fixed.

--- src/net/sf/memoranda/ui/PSP_NewTaskDialog.java
+++ src/net/sf/memoranda/ui/PSP_NewTaskDialog.java
@@ -61,7 +61,8 @@ public class PSP_NewTaskDialog extends JFrame implements Serializable {
 	 * Create the frame.
 	 */
 	public PSP_NewTaskDialog() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 589, 468);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
--- src/net/sf/memoranda/ui/PSP_NewTaskDialogClose.java
+++ src/net/sf/memoranda/ui/PSP_NewTaskDialogClose.java
@@ -40,7 +40,8 @@ public class PSP_NewTaskDialogClose extends JFrame {
 	 * Create the frame.
 	 */
 	public PSP_NewTaskDialogClose() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 536, 389);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
